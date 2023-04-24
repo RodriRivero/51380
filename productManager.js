c7271c2fd7da7838a284aca094983ca748a16019
rodrigoss88 <guidorodrigorivero@outlook.com>
2023-04-24 12:29:08 -0300

    pm

--- productManager.js
+++ productManager.js
@@ -1,78 +1,128 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor() {
-        this.products = []; // Arreglo vacío para almacenar los productos
+    constructor(path) {
         this.productId = 1; // Id autoincrementable para los productos
+        this.path = path;
     }
 
 
-    
     // Método para agregar un producto al arreglo de productos
-    addProduct(title, description, price, thumbnail, code, stock) {
+    async addProduct(file) { 
+        const {title, description, price, thumbnail, code, stock} = file;
         // Validación de campos obligatorios
-        if(!title || !description || !price || !thumbnail || !code || !stock) {
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
 
-            return console.log("Error: All fields are required.");
+            return `Error: All fields are required.`;
         }
 
         // Validación de código único
-        const existingProduct = this.products.find(product => product.code === code);
-        if(existingProduct) {
+        const products = await this.getProducts();
+        const existingProduct = products.find(product => product.code === code);
+        if (existingProduct) {
 
-            return console.log("Error: The product code already exists.");
+            return `Error: The product whith  code ${code} already exists.`; // pasarlo a string
         }
 
         // Creación de nuevo producto con id autoincrementable
         const newProduct = {
-            id: this.productId++, title, description, price, thumbnail, code, stock
+            id: this.productId ++,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
         };
 
         // Agregar producto al arreglo de productos
-        this.products.push(newProduct);
-        console.log("Product added successfully:", newProduct);
+        products.push(newProduct);
+        await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2))
+        return newProduct
     }
-
 
 
     // Método para obtener todos los productos
-    getProducts() {   
-        return console.log("All Products:",this.products);
+    async getProducts() {
+        if (!fs.existsSync(this.path)) {
+            return []
+        } else {
+            const fileData = await fs.promises.readFile(this.path, "utf-8")
+            const products = JSON.parse(fileData);
+            return products
+        }
+
     }
 
     // Método para buscar un producto por su id
-    getProductById(id) {
-        const product = this.products.find(product => product.id === id);
-        if(product) {
-            return  product
-        }
-        else {
-            return  `Error: Product with id ${id} not found .`
+    async getProductById(id) {
+        const products = await this.getProducts();
+        const product = products.find(product => product.id === id);
+        if (product) {
+            return product
+        } else {
+            return `Error: Product with id ${id} not found .`
         }
     }
+
+
+    // Método para actualizar un producto por su id
+    async updateProduct(id, updates) {
+        const products = await this.getProducts();
+        const index = products.findIndex(product => product.id === id);
+        if (index !== -1) {
+            const updatedProduct = {
+                ... products[index],
+                ...updates
+            };
+            products[index] = updatedProduct;
+            await fs.promises.writeFile(this.path, JSON.stringify(products));
+            console.log("Product updated successfully:", updatedProduct);
+            return updatedProduct;
+        } else {
+            console.log(`Error: Product with id ${id} not found.`);
+            return null;
+        }
+    }
+
+    // Método para eliminar un producto por su id
+    async deleteProduct(id) {
+        const products = await this.getProducts();
+        const index = products.findIndex(product => product.id === id);
+        if (index !== -1) {
+            const deletedProduct = products.splice(index, 1)[0];
+            await fs.promises.writeFile(this.path, JSON.stringify(products));
+            console.log("Product deleted successfully:", deletedProduct);
+            return deletedProduct;
+        } else {
+            console.log(`Error: Product with id ${id} not found.`);
+            return null;
+        }
+    }
+
 }
 
 
+const test = async () => {
 
-//*******************************/
+    const productManager = new ProductManager('file.json');
 
-// Ejemplo de uso de la clase ProductManager
+    console.log(await productManager.getProducts())
 
-// Crear una instancia de ProductManager
-const productManager = new ProductManager ();
+    const testProduct = {
+        title: "producto prueba",
+        description: "Este es un producto prueba 1",
+        price: 200,
+        thumbnail: "Sin imagen",
+        code: "abc123",
+        stock: 25
+    };
+    console.log(await productManager.addProduct(testProduct))
 
-// Agregar productos
+    console.log(await productManager.getProducts())
 
-productManager.addProduct("remera", "Descripción del producto 1", 9.99, "imagen1.jpg", "000", 10);
-productManager.addProduct("pantalon", "Descripción del producto 2", 19.99, "imagen2.jpg", "001", 5);
-productManager.addProduct("pantalon", "Descripción del producto 2", 19.99, "imagen2.jpg", "001", 5);
-productManager.addProduct("", "Descripción del producto 3", 14.99, "imagen3.jpg", "002", 3);
+    console.log(await productManager.getProductById(1))
+    console.log(await productManager.getProductById(19))
 
-// Obtener todos los productos
-const products = productManager.getProducts();
-//console.log("All Products:", products);
-
-console.log("Searc by Id:" , productManager.getProductById(77));
-
-// Buscar producto por id
-//const productById = productManager.getProductById(5);
-//console.log("Searc by Id");
-//console.log("Searc by Id:" , productManager.getProductById(5));
+}
+    test();
