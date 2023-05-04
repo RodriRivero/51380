be602043b579333106def7174c0a7b30284f1230
rodrigoss88 <guidorodrigorivero@outlook.com>
2023-05-04 01:49:41 -0300

    PE

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import ProductManager from "./src/components/ProductManager.js";
+import ProductManager from "./components/ProductManager.js";
 
 const app = express();
 const PORT = 8080;
